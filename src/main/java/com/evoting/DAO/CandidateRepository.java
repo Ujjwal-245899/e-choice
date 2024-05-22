969351a480a1a1bddb75b0120657c5d8a464b1aa
Ujjwal Kumar Singh <uks245899@gmail.com>
2024-05-22 12:44:06 +0530

    Changes FrontednSome Files

--- src/main/java/com/evoting/DAO/CandidateRepository.java
+++ src/main/java/com/evoting/DAO/CandidateRepository.java
@@ -2,6 +2,8 @@ package com.evoting.DAO;
 
 import com.evoting.Model.Candidate;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +12,8 @@ import java.util.List;
 public interface CandidateRepository extends JpaRepository<Candidate,Long> {
     List<Candidate> findByElectionId(Long electionId);
     Candidate findByid(Long id);
+
+
+    @Query("SELECT SUM(c.voteCount) FROM Candidate c WHERE c.election.id = :electionId")
+    Integer sumVoteCountByElectionId(@Param("electionId") Long electionId);
 }
